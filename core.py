9c7f432628fbe851f3f161052638f0598541506b
sweetfranklin <sweetfranklin99@gmail.com>
2023-11-07 18:52:56 +0800

    2023-11-07 18:52:56

--- core.py
+++ core.py
@@ -1,10 +1,10 @@
 
-            # 这是一个自动生成的Python文件
-            def hello_world():
-                print("Hello, world! Time is "Tue Nov  7 18:50:39 2023"")
+# 这是一个自动生成的Python文件
+def hello_world():
+    print("Hello, world! Time is 'Tue Nov  7 18:52:56 2023'")
 
 
-            if __name__ == "__main__":
-                hello_world()
-                a = 1
+if __name__ == "__main__":
+    hello_world()
+    b = 1
             
