843eceeaa653b8b9b82164aae8b998422f4c830a
sweetfranklin <140266175+sweetfranklin@users.noreply.github.com>
2023-11-11 22:50:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-
-# 这是一个自动生成的Python文件
-def hello_world():
-    print("Hello, world! Time is 'Fri Nov 10 15:10:35 2023'")
-
-
-if __name__ == "__main__":
-    hello_world()
-    b = 1
-             * \project	WonderTrader
+* \project	WonderTrader
  *
  * \author Wesley
